ac4976f309c7cfb97e11f47a0dd784e62d231f55
Arashk <arashkyoosha@gmail.com>
2024-11-15 18:50:52 -0500

    Some minor updates

--- Algorithms/Linear_Search/linear_search.py
+++ Algorithms/Linear_Search/linear_search.py
@@ -10,7 +10,7 @@ def verify(index):
     if index is not None:
         print("Target found at index: ", index)
     else:
-        print("Target not found in list")
+        print("Target not found in the list")
 
 numbers = [1,2,3,4,5,6,7,8,9,10]
 
