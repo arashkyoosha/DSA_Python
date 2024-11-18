c51a05fb0dc01574ffcff30ac2c6cd9bce77c91c
Arashk <arashkyoosha@gmail.com>
2024-11-17 22:02:46 -0500

    Updates added to hand on file

--- Algorithms/Linear_Search/linear_search_hands_on.py
+++ Algorithms/Linear_Search/linear_search_hands_on.py
@@ -1,6 +1,10 @@
-# Problem 1: Find a Number in a List
+"""
 
-"""Write a function that takes a list of integers and a target number as input. Use linear search to determine if the target number exists in the list. Return the index of the target if found; otherwise, return None."""
+Problem 1: Find a Number in a List
+
+Write a function that takes a list of integers and a target number as input. Use linear search to determine if the target number exists in the list. Return the index of the target if found; otherwise, return None.
+
+"""
 
 def find_value(list, target):
     for i in range(0, len(list)):
@@ -21,3 +25,102 @@ verify(result)
 
 result = find_value(numbers, 15)
 verify(result)
+
+
+"""
+
+Problem 2: Count Occurrences
+
+Given a list of integers, write a function to count how many times a specific number occurs in the list.
+
+
+"""
+
+def count_nums(list, target):
+    indices = []
+    for i in range(0, len(list)):
+        if list[i] == target:
+            indices.append(i)
+    return indices
+
+numbers = [1,2,3,2,5,7,2]
+target = 2
+print(count_nums(numbers, target))
+
+
+"""
+
+Problem 3: Find Maximum
+
+Write a function that takes a list of integers and returns the largest number in the list using linear search.
+
+"""
+
+def find_largest(list):    
+    largest = 0
+    for i in range(0, len(list)):            
+        if list[i] > largest:
+            largest = list[i]
+        return largest
+    
+
+numbers = [1,3,6,8,77,4,66,23,12]
+print(find_largest(numbers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Problem 4: Find Minimum in a Range
+
+Write a function that takes a list of integers and two indices (start and end) as input. Use linear search to find and return the smallest number in the sub-list between these indices (inclusive).
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Problem 5: Multiple Matches
+
+Given a list of integers, write a function to return a list of all indices where a specified number occurs. If the number doesn't appear, return an empty list.
+
+"""
