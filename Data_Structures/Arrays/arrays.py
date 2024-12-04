2e1cbf643f1d894fd0dd3b4f044d9f4c20b79d40
Arashk <arashkyoosha@gmail.com>
2024-12-03 19:02:28 -0500

    Arrays.py updated

--- Data_Structures/Arrays/arrays.py
+++ Data_Structures/Arrays/arrays.py
@@ -3,3 +3,26 @@ new_value = new_list[2]
 
 print(new_list)
 print(new_value)
+
+if 1 in new_list:
+    print(True)
+
+for n in new_list:
+    if n == 1:
+        print(True, "again!")
+
+        break
+
+new_list.append(4)
+print(new_list)
+
+
+numbers = []
+
+print(len(numbers))
+numbers.append(1)
+print(len(numbers))
+print(numbers)
+
+numbers.extend([2, 3 ,4 ,5 , 6])
+print(numbers)
