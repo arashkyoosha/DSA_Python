504bf208eef3782b968c4e5ab1a6fad91db5cba1
Arashk <arashkyoosha@gmail.com>
2024-12-04 20:55:01 -0500

    Updated linked list file

--- Data_Structures/Array_LinkedList/linked_list.py
+++ Data_Structures/Array_LinkedList/linked_list.py
@@ -11,7 +11,7 @@ class Node:
         self.data = data
 
     def __repr__(self):
-        return "<Node data: %s>" % self .data
+        return "<Node data: %s>" % self.data
     
 class LinkedList:
     """Singly linked list"""
@@ -32,4 +32,42 @@ class LinkedList:
             current = current.next_node
 
         return count
+    
+    def add(self, data):
+        """Adds a new node containing data at the head of the list - Takes O(1) time"""
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def search(self, key):
+        """Search for the first node containing data that matches the key
+        Returns the node or None if not found - Takes O(n) time"""
+
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+
+    def __repr__(self):
+        """Returns a string representation of the list - Takes O(n) time"""
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        return '-> '.join(nodes)
+    
+
     
